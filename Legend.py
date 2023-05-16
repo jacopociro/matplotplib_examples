44b8a35c5bb1fa70fd5f1e427c39f97b01b9d29c
jacopociro <jacopo.soncini@gmail.com>
2023-05-16 16:48:20 +0200

    progress-

--- Legend.py
+++ Legend.py
@@ -1,7 +1,8 @@
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
-
+from matplotlib.legend_handler import HandlerLine2D, HandlerTuple
+from numpy.random import randn
 fig,ax = plt.subplots()
 red_patch = mpatches.Patch(color='red', label='The red data')
 ax.legend(handles=[red_patch], bbox_to_anchor=(1,1), bbox_transform=fig.transFigure)
@@ -24,4 +25,70 @@ ax3_dict['bottom'].plot([3, 2, 1], label="test2")
 ax3_dict['bottom'].legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
 
 # figure legends
+fig4, axs4 = plt.subplot_mosaic([['left' , 'right']], layout='constrained')
+axs4['left'].plot([1, 2, 3], label="test1")
+axs4['left'].plot([3, 2, 1], label="test2")
+
+axs4['right'].plot([1, 2, 3], 'C2', label="test3")
+axs4['right'].plot([3, 2, 1], 'C3', label="test4")
+
+fig4.legend(loc='upper right')
+
+fig5, ax5 = plt.subplots(figsize=(6, 4), layout='constrained', facecolor='0.7')
+ax5.plot([1, 2], [1, 2], label='TEST')
+for loc in ['upper left', 'upper center', 'upper right', 'lower left', 'lower center', 'lower right']:
+    fig5.legend(loc=loc, title=loc)
+
+fig6, ax6 = plt.subplots(figsize=(6, 4), layout='constrained', facecolor='0.7')
+ax6.plot([1, 2], [1, 2], label='test')
+
+for loc in  ['center']:
+    fig6.legend(loc=loc, title=loc)
+
+# multiple legends on the sames axes
+fig7, ax7 = plt.subplots()
+line1, = ax7.plot([1, 2, 3], label="Line 1", linestyle='--')
+line2, = ax7.plot([3, 2, 1], label="Line 2", linewidth=4)
+
+first_legend = ax7.legend(handles=[line1], loc='upper right')
+
+ax7.add_artist(first_legend)
+ax.legend(handles=[line2], loc='lower right')
+
+# legend handlers
+fig8, ax8 = plt.subplots()
+line3, = ax8.plot([3, 2, 1], marker='o', label='Line1')
+line4, = ax8.plot([1, 2, 3], marker='o', label='Line 2')
+
+ax8.legend(handler_map={line1: HandlerLine2D(numpoints=4)})
+
+z = randn(10)
+fig9, ax9 = plt.subplots()
+red_dot, = ax9.plot(z, "ro", markersize=15)
+
+white_cross, =ax9.plot(z[:5], "w+", markeredgewidth=3, markersize=15)
+ax9.legend([red_dot, (red_dot, white_cross)], ["Attr A", "Attr A+B"])
+
+
+fig10, ax10 = plt.subplots()
+p1, = ax10.plot([1, 2.5, 3], "r-d")
+p2, =ax10.plot([3, 2, 1], "k-o")
+
+l = ax10.legend([(p1, p2)], ['Two keys'], numpoints=1, handler_map={tuple: HandlerTuple(ndivide=None)})
+
+# custom legend handler
+
+class AnyObject:
+    pass
+
+class AnyObjectHandler:
+    def legend_artist(self, legend, orig_handle, fontsize, handlebox):
+        x0, y0 = handlebox.xdescent, handlebox.ydescent
+        width, height = handlebox.width, handlebox.height
+        patch = mpatches.Rectangle([x0, y0], width, height, facecolor='red', edgecolor='black', hatch='xx', lw=3, transform=handlebox.get_transform())
+        handlebox.add_artist(patch)
+        return patch
+    
+fig11, ax11 = plt.subplots()
+ax11.legend([AnyObject()], ['My first Handler'], handler_map={AnyObject: AnyObjectHandler()})
 plt.show()
---
+++ constrained_layout.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt 
+import matplotlib.colors as mcolors
+import matplotlib.gridspec as gridspec
+import numpy as np
+
+plt.rcParams['savefig.facecolor'] = "0.8"
+plt.rcParams['figure.figsize'] = 4.5, 4.
+plt.rcParams['figure.max_open_warning'] = 50
+
+def example_plot(ax, fontsize=12, hide_labels=False):
+    ax.plot([1, 2])
+    ax.locator_params(nbins=3)
+    if hide_labels:
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+    else:
+        ax.set_xlabel('x-label', fontsize=fontsize)
+        ax.set_ylabel('y-label', fontsize=fontsize)
+        ax.set_title('Title', fontsize=fontsize)
+
+fig, ax = plt.subplots(layout="constrained")
+example_plot(ax, fontsize=24)
+
+# multiple subplots
+fig, axs = plt.subplots(2, 2, layout="constrained")
+
+for ax in axs.flat:
+    example_plot(ax)
+
+# colorbars
+arr = np.arange(100).reshape((10, 10))
+norm = mcolors.Normalize(vmin=0., vmax=100.)
+# pcolormesh consistent using
+pc_kwargs = {'rasterized': True, 'cmap': 'viridis',  'norm': norm}
+fig, axs = plt.subplots(3, 3, figsize=(4, 4), layout="constrained")
+for ax in axs.flat:
+    im = ax.pcolormesh(arr, **pc_kwargs)
+fig.colorbar(im, ax=axs[1:, ][:, 1], shrink=0.8)
+fig.colorbar(im, ax=axs[:, -1], shrink=0.6)
+
+# suptitle
+fig.suptitle('Big Suptitle')
+
+# legends
+
+fig, axs = plt.subplots(1, 2, figsize=(4,2),layout="constrained")
+
+axs[0].plot(np.arange(10))
+axs[1].plot(np.arange(10), label='this is a plot')
+
+axs[1].legend(loc='center left', bbox_to_anchor=(0.8,0.5))
+
+plt.show()
